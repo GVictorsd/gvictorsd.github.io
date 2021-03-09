6de7ae5f13f91e7bb70ffae63e0a61af455af11a
Victor <victorswaroop2002@gmail.com>
2021-03-09 15:43:14 +0530

    updated contact

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-		function FadeInSection(props) {
+function FadeInSection(props) {
 		  const [isVisible, setVisible] = React.useState(true);
 		  const domRef = React.useRef();
 		  React.useEffect(() => {
@@ -17,3 +17,22 @@
 		    </div>
 		  );
 		}
+
+
+function isElementInViewport (el) {
+
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
+
