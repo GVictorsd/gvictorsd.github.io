22312cf8cb7d6cf55017e08dc8814428d14fdea0
Victor <victorswaroop2002@gmail.com>
2021-06-29 22:31:37 +0530

    improved theming

--- script/main.js
+++ script/main.js
@@ -38,3 +38,34 @@ document.addEventListener('scroll', function () {
     passive: true
 });
 */
+
+
+const switcher = document.getElementById('theme')
+const doc = document.firstElementChild;
+
+
+switcher.addEventListener('input', e =>{
+ 		setTheme(e.target.value);
+});
+
+const setTheme = theme => {
+
+	//get stored theme from local storage
+	let storedTheme = localStorage.getItem('storedTheme');
+
+	if(storedTheme == null){
+		// if theme was not stored previously
+		// store and apply
+		localStorage.setItem('storedTheme', 'auto');
+		doc.setAttribute('color-scheme', 'auto');
+	}else{
+		// else apply the found theme...
+		localStorage.setItem('storedTheme', theme);
+		doc.setAttribute('color-scheme', theme);
+	}
+	
+}
+
+// get stored theme and apply it
+let storedTheme = localStorage.getItem('storedTheme');
+doc.setAttribute('color-scheme', storedTheme);
