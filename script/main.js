ed12525fd364114854f3e973c11884877eeab855
Victor <victorswaroop2002@gmail.com>
2021-07-04 18:58:38 +0530

    fixed a bug

--- script/main.js
+++ script/main.js
@@ -1,4 +1,7 @@
+
+
 function isInViewport(el) {
+	/* returns if an element is visible.. */
     const rect = el.getBoundingClientRect();
     return (
         rect.top >= 0 &&
@@ -10,17 +13,14 @@ function isInViewport(el) {
 }
 
 
+/* Make floating contact button visible if top contact icon is not visible... */
 const box = document.querySelector('#contactMe');
-//const message = document.querySelector('#message');
 const elem = document.querySelector('#floating-contact');
 
 document.addEventListener('scroll', function () {
     isInViewport(contactMe) ?
 		elem.style.visibility = 'hidden':// it is visible...
         elem.style.visibility = 'visible'; //not visible...
-
-//    message.textContent = messageText;
-
 },{
     passive: true
 });
@@ -38,34 +38,3 @@ document.addEventListener('scroll', function () {
     passive: true
 });
 */
-
-
-const switcher = document.getElementById('theme')
-const doc = document.firstElementChild;
-
-
-switcher.addEventListener('input', e =>{
- 		setTheme(e.target.value);
-});
-
-const setTheme = theme => {
-
-	//get stored theme from local storage
-	let storedTheme = localStorage.getItem('storedTheme');
-
-	if(storedTheme == null){
-		// if theme was not stored previously
-		// store and apply
-		localStorage.setItem('storedTheme', 'auto');
-		doc.setAttribute('color-scheme', 'auto');
-	}else{
-		// else apply the found theme...
-		localStorage.setItem('storedTheme', theme);
-		doc.setAttribute('color-scheme', theme);
-	}
-	
-}
-
-// get stored theme and apply it
-let storedTheme = localStorage.getItem('storedTheme');
-doc.setAttribute('color-scheme', storedTheme);
