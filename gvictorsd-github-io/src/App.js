785c0ddb52140f271c734880148e98512910efa7
victorswaroop.das <victorswaroop.das@synergymachines.us>
2023-09-22 17:06:14 +0530

    scroll snap

--- gvictorsd-github-io/src/App.js
+++ gvictorsd-github-io/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Section from './components/section/section';
 import back1 from './assets/back1.jpg';
+import back2 from './assets/back2.jpg';
+import back3 from './assets/back3.jpg';
 
 function App() {
   return (
@@ -9,7 +11,13 @@ function App() {
         <h1>React 18 Alpha</h1>
       </Section>
 
-      <Section imageuri={back1}>
+      <Section imageuri={back2}>
+        <div>
+          <h1>Hello world</h1>
+        </div>
+      </Section>
+
+      <Section imageuri={back3}>
         <div>
           <h1>Hello world</h1>
         </div>
